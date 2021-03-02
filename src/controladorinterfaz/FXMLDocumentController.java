890a2452080e4f38067bfc31f8b510a7ee7a13c1
mangel119 <mangel.sanabria119@gmail.com>
2021-03-01 23:08:09 -0500

    Dados1

--- src/controladorinterfaz/FXMLDocumentController.java
+++ src/controladorinterfaz/FXMLDocumentController.java
@@ -6,11 +6,15 @@
 package controladorinterfaz;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import sun.net.www.content.image.png;
 
 /**
  *
@@ -19,12 +23,70 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private Label labeltitulo;
+    @FXML
+    private ImageView dadouno;
+    @FXML
+    private ImageView dadodos;
+    
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        Random rnd = new Random();
+        int dado1 = 0;
+        dado1 = ((int)(rnd.nextDouble()*6));
+        int dado2 = 0;
+        dado2 = ((int)(rnd.nextDouble()*6));
+        
+        if (dado1==0){
+            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/1.png")));
+        }
+        else if (dado1==1)
+        {
+            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/2.png")));
+        }
+        else if (dado1==2)
+        {
+            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/3.png")));
+        }
+        else if (dado1==3)
+        {
+            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/4.png")));
+        }
+        else if (dado1==4)
+        {
+            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/5.png")));
+        }
+        else if (dado1==5)
+        {
+            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/6.png")));
+        }
+        
+        
+        if (dado2==0){
+            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/1.png")));
+        }
+        else if (dado2==1)
+        {
+            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/2.png")));
+        }
+        else if (dado2==2)
+        {
+            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/3.png")));
+        }
+        else if (dado2==3)
+        {
+            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/4.png")));
+        }
+        else if (dado2==4)
+        {
+            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/5.png")));
+        }
+        else if (dado2==5)
+        {
+            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/6.png")));
+        }
     }
     
     @Override
--- src/vista/TallerGrupo.java
+++ src/vista/TallerGrupo.java
@@ -17,6 +17,8 @@ import javafx.stage.Stage;
  */
 public class TallerGrupo extends Application {
     
+    
+    
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
