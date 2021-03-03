3514946a0142693b190b5d88bf11ee1fcfff101d
mangel119 <mangel.sanabria119@gmail.com>
2021-03-03 14:53:02 -0500

    Solución Error SetImage

--- src/controladorinterfaz/FXMLDocumentController.java
+++ src/controladorinterfaz/FXMLDocumentController.java
@@ -34,59 +34,65 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         Random rnd = new Random();
-        int dado1 = 0;
+        int dado1 = 1;
         dado1 = ((int)(rnd.nextDouble()*6));
-        int dado2 = 0;
-        dado2 = ((int)(rnd.nextDouble()*6));
-        
-        if (dado1==0){
-            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/1.png")));
-        }
-        else if (dado1==1)
+        int dado2 = 1;
+        dado2 =((int)(rnd.nextDouble()*6));
+
+         if (dado1==1)
         {
-            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/2.png")));
+            Image imagen=new Image("/imagenes/1.png");
+            dadouno.setImage(imagen);
         }
         else if (dado1==2)
         {
-            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/3.png")));
+           Image imagen=new Image("/imagenes/2.png");
+            dadouno.setImage(imagen);
         }
         else if (dado1==3)
         {
-            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/4.png")));
+            Image imagen=new Image("/imagenes/3.png");
+            dadouno.setImage(imagen);
         }
         else if (dado1==4)
         {
-            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/5.png")));
+           Image imagen=new Image("/imagenes/4.png");
+            dadouno.setImage(imagen);
         }
         else if (dado1==5)
         {
-            dadouno.setImage(new Image(getClass().getResourceAsStream("imagenes/6.png")));
+            Image imagen=new Image("/imagenes/5.png");
+            dadouno.setImage(imagen);
         }
         
         
-        if (dado2==0){
-            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/1.png")));
-        }
-        else if (dado2==1)
+         if (dado2==1)
         {
-            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/2.png")));
+            Image imagen=new Image("/imagenes/1.png");
+            dadodos.setImage(imagen);
         }
-        else if (dado2==2)
+         
+         else if (dado2==2)
         {
-            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/3.png")));
+           Image imagen=new Image("/imagenes/2.png");
+            dadodos.setImage(imagen);
         }
         else if (dado2==3)
         {
-            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/4.png")));
+            Image imagen=new Image("/imagenes/3.png");
+            dadodos.setImage(imagen);
         }
         else if (dado2==4)
         {
-            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/5.png")));
+           Image imagen=new Image("/imagenes/4.png");
+            dadodos.setImage(imagen);
         }
         else if (dado2==5)
         {
-            dadodos.setImage(new Image(getClass().getResourceAsStream("imagenes/6.png")));
+            Image imagen=new Image("/imagenes/5.png");
+            dadodos.setImage(imagen);
         }
+         
     }
     
     @Override
