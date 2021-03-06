ed55c7ebacf916ee119fe93ca855887bb74799ff
mangel119 <mangel.sanabria119@gmail.com>
2021-03-05 21:23:00 -0500

    Asesoria

--- src/controladorinterfaz/FXMLDocumentController.java
+++ src/controladorinterfaz/FXMLDocumentController.java
@@ -26,7 +26,7 @@ import sun.net.www.content.image.png;
  * @author Familia Sanabria
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Label labeltitulo;
     @FXML
@@ -36,85 +36,66 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private WebView tabla;
     private WebEngine engine;
-    
-    Pila<Lanzamiento>pilad;
-    
-    
+    int dado2;
+    int dado1;
+    Pila<Lanzamiento> pilad;
+
     @FXML
     private void lanzar(ActionEvent event) {
         Random rnd = new Random();
-        int dado1 = 1;
-        dado1 = ((int)(rnd.nextDouble()*6));
-        int dado2 = 1;
-        dado2 =((int)(rnd.nextDouble()*6));
 
-         if (dado1==1)
-        {
-            Image imagen=new Image("/imagenes/1.png");
+        dado1 = ((int) (rnd.nextDouble() * 6));
+        System.out.println("numero 1" + dado1);
+
+        dado2 = ((int) (rnd.nextDouble() * 6));
+        System.out.println("numero 2" + dado2);
+        if (dado1 == 1) {
+            Image imagen = new Image("/imagenes/1.png");
+            dadouno.setImage(imagen);
+        } else if (dado1 == 2) {
+            Image imagen = new Image("/imagenes/2.png");
+            dadouno.setImage(imagen);
+        } else if (dado1 == 3) {
+            Image imagen = new Image("/imagenes/3.png");
+            dadouno.setImage(imagen);
+        } else if (dado1 == 4) {
+            Image imagen = new Image("/imagenes/4.png");
+            dadouno.setImage(imagen);
+        } else if (dado1 == 5) {
+            Image imagen = new Image("/imagenes/5.png");
             dadouno.setImage(imagen);
         }
-        else if (dado1==2)
-        {
-           Image imagen=new Image("/imagenes/2.png");
-            dadouno.setImage(imagen);
-        }
-        else if (dado1==3)
-        {
-            Image imagen=new Image("/imagenes/3.png");
-            dadouno.setImage(imagen);
-        }
-        else if (dado1==4)
-        {
-           Image imagen=new Image("/imagenes/4.png");
-            dadouno.setImage(imagen);
-        }
-        else if (dado1==5)
-        {
-            Image imagen=new Image("/imagenes/5.png");
-            dadouno.setImage(imagen);
-        }
-        
-        
-         if (dado2==1)
-        {
-            Image imagen=new Image("/imagenes/1.png");
+
+        if (dado2 == 1) {
+            Image imagen = new Image("/imagenes/1.png");
+            dadodos.setImage(imagen);
+        } else if (dado2 == 2) {
+            Image imagen = new Image("/imagenes/2.png");
+            dadodos.setImage(imagen);
+        } else if (dado2 == 3) {
+            Image imagen = new Image("/imagenes/3.png");
+            dadodos.setImage(imagen);
+        } else if (dado2 == 4) {
+            Image imagen = new Image("/imagenes/4.png");
+            dadodos.setImage(imagen);
+        } else if (dado2 == 5) {
+            Image imagen = new Image("/imagenes/5.png");
             dadodos.setImage(imagen);
         }
-         
-         else if (dado2==2)
-        {
-           Image imagen=new Image("/imagenes/2.png");
-            dadodos.setImage(imagen);
-        }
-        else if (dado2==3)
-        {
-            Image imagen=new Image("/imagenes/3.png");
-            dadodos.setImage(imagen);
-        }
-        else if (dado2==4)
-        {
-           Image imagen=new Image("/imagenes/4.png");
-            dadodos.setImage(imagen);
-        }
-        else if (dado2==5)
-        {
-            Image imagen=new Image("/imagenes/5.png");
-            dadodos.setImage(imagen);
-        }
-         
-         
-         pilad.apilar(new Lanzamiento(dado1, dado2));
-         
+
+        pilad.apilar(new Lanzamiento(dado1, dado2));
+
         engine.loadContent(Tools.convertirPilaAHtml(pilad));
-         
-         
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        pilad=new Pila<>();
+        pilad = new Pila<>();
         engine = tabla.getEngine();
-    }    
-    
+        dado2 = 1;
+        dado1 = 1;
+    }
+
 }
